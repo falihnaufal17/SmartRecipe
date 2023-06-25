0779cfff4031f7e7f78d87cdfd90d013028f57e4
todayisvin <kevinpratamaoke@gmail.com>
2023-06-25 22:06:32 +0700

    feat: create translate feature on recipe.js

--- src/screens/Recipe.js
+++ src/screens/Recipe.js
@@ -1,11 +1,12 @@
 import { useFocusEffect } from '@react-navigation/native';
 import axios from 'axios'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useState, useEffect } from 'react'
 import { Alert, View, StyleSheet, ActivityIndicator, Image, ScrollView, SafeAreaView, TouchableOpacity, ToastAndroid } from 'react-native'
 import { firstLetterCapital } from '../helper';
 import {Text, MD3Colors} from 'react-native-paper'
 import { useAuth } from '../contexts/Auth';
 import { BASE_API_URL } from '../constants/general';
+import MLKitTranslator, { LANG_TAGS } from '../MLKitTranslator';
 
 const Recipe = ({ route }) => {
   const { id } = route.params
@@ -31,9 +32,12 @@ const Recipe = ({ route }) => {
     }
   }
 
+  useEffect(() => {
+    fetchDetail();
+  }, []);
+
   useFocusEffect(
     useCallback(() => {
-
       fetchDetail()
 
       return () => {
@@ -41,16 +45,6 @@ const Recipe = ({ route }) => {
       }
     }, [id])
   )
-  
-
-  if (loading) {
-    return (
-      <View style={styles.containerLoading}>
-        <ActivityIndicator />
-        <Text>Loading...</Text>
-      </View>
-    )
-  }
 
   const addToBookmark = async () => {
     try {
@@ -82,6 +76,100 @@ const Recipe = ({ route }) => {
     }
   }
 
+  const translateTextToIndonesian = async (text) => {
+    try {
+      const sourceLang = await MLKitTranslator.identifyLanguage(text);
+      const targetLang = LANG_TAGS.INDONESIAN;
+      const isSourceLangDownloaded = await MLKitTranslator.isModelDownloaded(sourceLang);
+      const isTargetLangDownloaded = await MLKitTranslator.isModelDownloaded(targetLang);
+
+      if (!isSourceLangDownloaded || !isTargetLangDownloaded) {
+        Alert.alert('Terjemahan', 'Model bahasa belum tersedia, sedang mengunduh... Anda akan diberitahu lagi setelah berhasil diunduh');
+        await MLKitTranslator.downloadModel(sourceLang);
+        await MLKitTranslator.downloadModel(targetLang);
+        Alert.alert('Terjemahan', 'Unduhan berhasil');
+      } else {
+        const translatedText = await MLKitTranslator.translateText(text, sourceLang, targetLang);
+        return translatedText;
+      }
+    } catch (error) {
+      Alert.alert('Error', error.message);
+    }
+  }
+
+  const translateIngredients = async () => {
+    const translatedIngredients = await Promise.all(detail?.ingredients?.map(async (item) => {
+      const translatedName = await translateTextToIndonesian(item.name);
+      const translatedAmount = await translateTextToIndonesian(`${item.amount.metric.value} ${item.amount.metric.unit}`);
+      return `- ${firstLetterCapital(translatedName)} (${translatedAmount})`;
+    }));
+    return translatedIngredients;
+  }
+
+  const translateEquipments = async () => {
+    const translatedEquipments = await Promise.all(detail?.equipments?.map(async (item) => {
+      const translatedName = await translateTextToIndonesian(item.name);
+      return `- ${firstLetterCapital(translatedName)}`;
+    }));
+    return translatedEquipments;
+  }
+
+  const translateInstructions = async () => {
+    const translatedInstructions = await Promise.all(detail?.instructions?.map(async (item, key) => {
+      let translatedSteps = [];
+      if (item?.name?.length > 0) {
+        const translatedName = await translateTextToIndonesian(item.name);
+        translatedSteps.push(`${key + 1}. ${translatedName}`);
+      }
+      const translatedStepItems = await Promise.all(item.steps.map(async (step) => {
+        const translatedStepNumber = await translateTextToIndonesian(step.number);
+        const translatedStep = await translateTextToIndonesian(step.step);
+        return `${translatedStepNumber}. ${translatedStep}`;
+      }));
+      translatedSteps = translatedSteps.concat(translatedStepItems);
+      return translatedSteps.join('\n');
+    }));
+    return translatedInstructions;
+  }
+
+  const fetchTranslatedDetail = async () => {
+    setLoading(true);
+
+    try {
+      const translatedIngredients = await translateIngredients();
+      const translatedEquipments = await translateEquipments();
+      const translatedInstructions = await translateInstructions();
+
+      const translatedDetail = {
+        ...detail,
+        ingredients: translatedIngredients,
+        equipments: translatedEquipments,
+        instructions: translatedInstructions
+      };
+
+      setDetail(translatedDetail);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      Alert.alert('Error', 'Gagal menerjemahkan detail resep');
+    }
+  }
+
+  useEffect(() => {
+    if (detail) {
+      fetchTranslatedDetail();
+    }
+  }, [detail]);
+
+  if (loading) {
+    return (
+      <View style={styles.containerLoading}>
+        <ActivityIndicator />
+        <Text>Loading...</Text>
+      </View>
+    )
+  }
+
   return (
     <SafeAreaView>
       <ScrollView contentContainerStyle={styles.container}>
@@ -99,26 +187,21 @@ const Recipe = ({ route }) => {
           ) : null}
         </View>
         <View style={styles.contentSection}>
-          <Text style={styles.contentTitle}>Ingredients:</Text>
+          <Text style={styles.contentTitle}>Bahan-bahan:</Text>
           {detail?.ingredients?.map((item, key) => (
-            <Text style={styles.contentItem} key={key}>- {firstLetterCapital(item.name)} ({item.amount.metric.value} {item.amount.metric.unit})</Text>
+            <Text style={styles.contentItem} key={key}>{item}</Text>
           ))}
         </View>
         <View style={styles.contentSection}>
-          <Text style={styles.contentTitle}>Equipments:</Text>
+          <Text style={styles.contentTitle}>Peralatan:</Text>
           {detail?.equipments?.map((item, key) => (
-            <Text style={styles.contentItem} key={key}>- {firstLetterCapital(item.name)}</Text>
+            <Text style={styles.contentItem} key={key}>{item}</Text>
           ))}
         </View>
         <View style={styles.contentSection}>
-          <Text style={styles.contentTitle}>Instructions:</Text>
+          <Text style={styles.contentTitle}>Instruksi:</Text>
           {detail?.instructions?.map((item, key) => (
-            <>
-              {item?.name?.length > 0 ? <Text style={styles.contentItem} key={key}>{key + 1}. {item.name}</Text> : null}
-              {item.steps.map((step, stepKey) => (
-                <Text style={styles.contentItem} key={stepKey}>{step.number}. {step.step}</Text>
-              ))}
-            </>
+            <Text style={styles.contentItem} key={key}>{item}</Text>
           ))}
         </View>
       </ScrollView>
