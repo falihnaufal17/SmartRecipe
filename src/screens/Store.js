4b8f85b14a8b67db213ec43bf04f9101e0b1a35a
falihnaufal17 <falihnaufal1700@gmail.com>
2023-08-10 23:40:13 +0700

    fix: turn on gps when gps is off

--- src/screens/Store.js
+++ src/screens/Store.js
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Text, StyleSheet, Alert, TouchableOpacity, Linking, PermissionsAndroid, ScrollView, SafeAreaView } from 'react-native'
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { Text, StyleSheet, Alert, TouchableOpacity, Linking, PermissionsAndroid, ScrollView, SafeAreaView, View } from 'react-native'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { PLACES } from '../constants/store';
 import axios from 'axios'
@@ -7,8 +7,15 @@ import { API_KEY_GOOGLE_MAPS, BASE_URL_PLACES_API } from '../constants/general';
 import Geocoder from 'react-native-geocoding';
 import Geolocation from '@react-native-community/geolocation';
 import { useFocusEffect } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import LocationEnabler from "react-native-location-enabler"
 
 Geocoder.init(API_KEY_GOOGLE_MAPS);
+
+const {
+  PRIORITIES: { HIGH_ACCURACY },
+  useLocationSettings,
+} = LocationEnabler
 
 export default function Store() {
   const [region, setRegion] = useState({
@@ -20,6 +27,16 @@ export default function Store() {
   const [activePlace, setActivePlace] = useState('')
   const [dataPlaces, setDataPlaces] = useState([])
   const [originPlace, setOriginPlace] = useState('')
+  const [enabled, requestResolution] = useLocationSettings(
+    {
+      priority: HIGH_ACCURACY, // default BALANCED_POWER_ACCURACY
+      alwaysShow: true, // default false
+      needBle: true, // default false
+    },
+    false /* optional: default undefined */,
+  )
+  const [isActiveLocation, setIsActiveLocation] = useState(false)
+  const mapRef = useRef()
 
   useFocusEffect(
     useCallback(() => {
@@ -63,6 +80,8 @@ export default function Store() {
   };
 
   const getCurrentPosition = async () => {
+    requestResolution()
+
     if (await hasLocationPermission()) {
       Geolocation.getCurrentPosition(
         position => {
@@ -71,6 +90,12 @@ export default function Store() {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
           }));
+          mapRef.current.animateToRegion({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121,
+          })
           Geocoder.from(position.coords.latitude, position.coords.longitude)
             .then(json => {
               const addressComponent = json.results[0].address_components[0];
@@ -130,12 +155,17 @@ export default function Store() {
     );
   }
 
-  const onOpenDirection = (destination) => {
-    const originFormatted = originPlace.replace(' ', '+')
-    const destinationFormatted = destination?.replace(' ', '+')
+  // const onOpenDirection = (destination) => {
+  //   const originFormatted = originPlace.replace(' ', '+')
+  //   const destinationFormatted = destination?.replace(' ', '+')
 
-    Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=${originFormatted}&destination=${destinationFormatted}`)
-  }
+  //   Linking.openURL(`https://www.google.com/maps/dir/?api=1&origin=${originFormatted}&destination=${destinationFormatted}`)
+  // }
+
+  const setCurrentPosition = useCallback(() => {
+    getCurrentPosition()
+    setIsActiveLocation(true)
+  }, [getCurrentPosition, isActiveLocation])
 
   return (
     <SafeAreaView style={styles.container}>
@@ -145,19 +175,34 @@ export default function Store() {
           const activeItemLabelStyle = item.value === activePlace ? { ...styles.placeItemLabelActive } : {}
 
           return (
-            <TouchableOpacity activeOpacity={0.8} onPress={() => onChoosePlace(item.value)} style={[styles.placeItem, { ...activeItemStyle }]} key={key}>
+            <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() => onChoosePlace(item.value)}
+              style={[
+                styles.placeItem,
+                { ...activeItemStyle }
+              ]}
+              key={key}
+            >
               <Text style={[styles.placeItemLabel, { ...activeItemLabelStyle }]}>{item.label}</Text>
             </TouchableOpacity>
           )
         })}
       </ScrollView>
+      <TouchableOpacity
+        style={styles.btnGetCurrentLocation}
+        onPress={setCurrentPosition}
+      >
+        <Icon name="target" size={30} color={isActiveLocation ? "#3f50b5" : "#7A7A7A"} />
+      </TouchableOpacity>
       <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
         style={styles.map}
-        showsMyLocationButton={true}
+        showsMyLocationButton={false}
         showsUserLocation={true}
         zoomControlEnabled={true}
         region={region}
+        ref={mapRef}
       >
         {dataPlaces.length > 0 ? dataPlaces.map((item, key) => (
           <Marker
@@ -213,5 +258,24 @@ const styles = StyleSheet.create({
   },
   placeItemLabelActive: {
     color: '#FFF',
+  },
+  btnGetCurrentLocation: {
+    position: 'absolute',
+    bottom: '15%',
+    right: 9,
+    backgroundColor: '#FFF',
+    zIndex: 3,
+    width: 42,
+    height: 42,
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+    elevation: 1,
+    opacity: 0.99
+  },
+  txtGetCurrentLocation: {
+    color: '#000'
   }
 })
